fa42c82a01452f0c0d603662a9d7117eacd34700
Joakim Jacobsen <67727027+JJacobsen441@users.noreply.github.com>
2023-12-01 05:49:14 +0100

    Initial commit

---
+++ Awesome.AI/Common/Stats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Awesome.AI.Common
+{
+    public class Stat
+    {
+        public string name { get; set; }
+        public int count_all { get; set; }
+        public double force { get; set; }
+        public double conv_index { get; set; }
+    }
+
+    public class Stats
+    {
+        public string curr_name { get; set; }
+        public int curr_value { get; set; }
+        public string reset_name { get; set; }
+        public int reset_value { get; set; }
+
+        public List<Stat> list = new List<Stat>();
+    }
+
+}
