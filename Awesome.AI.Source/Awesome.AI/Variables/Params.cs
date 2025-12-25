0088f0b9dc49fa12fd3135c5076d092bcbe22261
Joakim Jacobsen <67727027+JJacobsen441@users.noreply.github.com>
2025-12-26 00:28:37 +0100

    Cleanup
    cleanup
    
    Cleanup
    
    cleanup

--- Awesome.AI.Source/Awesome.AI/Variables/Params.cs
+++ Awesome.AI.Source/Awesome.AI/Variables/Params.cs
@@ -18,44 +18,61 @@ namespace Awesome.AI.Variables
         public IMechanics Mechanics(MECHANICS run)
         {
             /*
-                * INFO (not used)
-                * earth mass:      5.972 × 10^24 kg
-                * sun mass:        1.989 × 10^30 kg
-                * earth radius:            6,371 km
-                * distance moon:         384,400 km
-                * distance sun:      148.010.000 km
-                * car mass:                  500 kg
-                * */
+             * INFO (not used)
+             * earth mass:      5.972 × 10^24 kg
+             * sun mass:        1.989 × 10^30 kg
+             * earth radius:            6,371 km
+             * distance moon:         384,400 km
+             * distance sun:      148.010.000 km
+             * car mass:                  500 kg
+             * */
 
             switch (run)
             {
-                // low
                 case MECHANICS.TUGOFWAR_LOW:
-                case MECHANICS.BALLONHILL_LOW: 
+                case MECHANICS.BALLONHILL_LOW:
+                    validation = VALIDATION.BOTH;                                       //BOTH or OCCU
+                    tags = TAGS.ALL;                                                    //used with TAGS and BOTH
+                    occupasion = OCCUPASION.DYNAMIC;                                    //used with OCCU and BOTH
+                    pattern = PATTERN.MOODGENERAL;
                     return new m_NoiseGenerator(mind, run);
                 case MECHANICS.CIRCUIT_1_LOW:
-                case MECHANICS.CIRCUIT_2_LOW: 
+                case MECHANICS.CIRCUIT_2_LOW:
+                    validation = VALIDATION.BOTH;                                       //BOTH or OCCU
+                    tags = TAGS.ALL;                                                    //used with TAGS and BOTH
+                    occupasion = OCCUPASION.DYNAMIC;                                    //used with OCCU and BOTH
+                    pattern = PATTERN.MOODGENERAL;
                     return new e_CircuitSimulator(mind, run);
-
-                case MECHANICS.TUGOFWAR_HIGH: 
+                case MECHANICS.TUGOFWAR_HIGH:
+                    validation = VALIDATION.BOTH;                                       //BOTH or OCCU
+                    tags = TAGS.ALL;                                                    //used with TAGS and BOTH
+                    occupasion = OCCUPASION.DYNAMIC;                                    //used with OCCU and BOTH
+                    pattern = PATTERN.MOODGENERAL;
                     return new m_TugOfWar(mind, PROPS.COMMUNICATION);
-                case MECHANICS.BALLONHILL_HIGH: 
+                case MECHANICS.BALLONHILL_HIGH:
+                    validation = VALIDATION.BOTH;                                       //BOTH or TAGS
+                    tags = TAGS.ALL;                                                    //used with TAGS and BOTH
+                    occupasion = OCCUPASION.DYNAMIC;                                    //used with OCCU and BOTH
+                    pattern = PATTERN.MOODGENERAL; 
                     return new m_BallOnHill(mind, PROPS.BRAINWAVE);
-                
                 //case MECHANICS.GRAVITY:
-                //return new GravityAndRocket(mind, this);
-                default: 
-                    throw new Exception("GetMechanics");
+                //    _mech = new GravityAndRocket(mind, this);
+                //    validation = VALIDATION.BOTH;                                       //BOTH or OCCU
+                //    tags = TAGS.ALL;                                                    //used with TAGS and BOTH
+                //    occupasion = OCCUPASION.DYNAMIC;                                    //used with OCCU and BOTH
+                //    pattern = PATTERN.MOODGENERAL;
+                //    break;
+                default: throw new Exception("GetMechanics");
             }
-        }
+        }        
 
         /*
          * VARIABLE parameters
          * */
 
-        public VALIDATION validation = VALIDATION.BOTH;     //BOTH or TAGS
-        public OCCUPASION occupasion = OCCUPASION.DYNAMIC;  //used with OCCU and BOTH
-        public PATTERN pattern = PATTERN.MOODGENERAL;       
-        public TAGS tags = TAGS.ALL;                        //used with TAGS and BOTH
+        public VALIDATION validation;
+        public TAGS tags;                                               //used with WORLD and BOTH
+        public OCCUPASION occupasion;                                   //used with SELF and BOTH
+        public PATTERN pattern;
     }
 }
